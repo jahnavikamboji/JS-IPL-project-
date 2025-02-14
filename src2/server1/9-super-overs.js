581e94e2691aee121ca021962a85999fb7cb55f8
jahnavi-649 <jahnavikamboji@gmail.com>
2025-02-14 10:29:23 +0530

    deleting files

--- src2/server1/9-super-overs.js
+++ src2/server1/9-super-overs.js
@@ -2,40 +2,46 @@ const csvToJson = require("csvtojson");
 const fs = require("fs");
 const path = require("path");
 
-const deliveriesFilePath = path.join("../Data/deliveries.csv");
+const deliveriesFilePath = path.join("../../Data/deliveries.csv");
 
 // Find the bowler with the best economy in super overs.
 function superOverDeliveries() {
     csvToJson().fromFile(deliveriesFilePath).then((data) => {
-        let bowlers = data.reduce((acc, curr) => {
+        let bowlers = {};
+
+        for (const curr of data) {
             if (curr.is_super_over == 1) {  
                 let bowler = curr.bowler;
                 let wideRuns = +curr.wide_runs;
                 let noBallRuns = +curr.noball_runs;
                 let batsmanRuns = +curr.batsman_runs;
                 
-                if (!acc[bowler]) {
-                    acc[bowler] = {
+                if (!bowlers[bowler]) {
+                    bowlers[bowler] = {
                         no_of_balls: wideRuns > 0 || noBallRuns > 0 ? 0 : 1,
                         no_of_runs: wideRuns + noBallRuns + batsmanRuns
                     };
                 } else {
-                    acc[bowler].no_of_balls += wideRuns > 0 || noBallRuns > 0 ? 0 : 1;
-                    acc[bowler].no_of_runs += wideRuns + noBallRuns + batsmanRuns;
+                    bowlers[bowler].no_of_balls += wideRuns > 0 || noBallRuns > 0 ? 0 : 1;
+                    bowlers[bowler].no_of_runs += wideRuns + noBallRuns + batsmanRuns;
                 }
             }
-            return acc;
-        }, {});
+        }
 
-        let superEconomy = Object.entries(bowlers)
-            .map(([bowler, stats]) => ({
-                bowler: bowler,
-                economy: ((stats.no_of_runs * 6) / stats.no_of_balls).toFixed(2)
-            }))
-            .sort((a, b) => a.economy - b.economy);
-     
+        let superEconomy = [];
+        for (const bowler in bowlers) {
+            if (bowlers[bowler].no_of_balls > 0) {
+                superEconomy.push({
+                    bowler: bowler,
+                    economy: ((bowlers[bowler].no_of_runs * 6) / bowlers[bowler].no_of_balls).toFixed(2)
+                });
+            }
+        }
+
+        superEconomy.sort((a, b) => a.economy - b.economy);
+        
         let bestBowler = superEconomy[0] || {};
-        fs.writeFileSync("../public1/output/9-super-overs.json", JSON.stringify(bestBowler, null, 2));
+        fs.writeFileSync("../../public2/public1/output/9-super-overs.json", JSON.stringify(bestBowler, null, 2));
 
         console.log(bestBowler);
     });
