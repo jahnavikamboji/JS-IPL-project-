ed4b50184ef7b2fced766911441b72fcc28659f0
jahnavi-649 <jahnavikamboji@gmail.com>
2025-02-18 09:55:09 +0530

    changes

--- server/1-matches-per-year.js
+++ server/1-matches-per-year.js
@@ -2,10 +2,10 @@ const csvToJson = require("csvtojson");
 const fs= require("fs");
 
 //Number of matches played per year for all the years in IPL.
-csvToJson().fromFile("../Data/matches.csv").then(source => {
-    function matchesPerYear(source){
+csvToJson().fromFile("../Data/matches.csv").then(matches => {
+    function matchesPerYear(matches){
         // console.log(source);
-        const matchesData = source.reduce((acc, match) => {
+        const matchesData = matches.reduce((acc, match) => {
             acc[match.season] = (acc[match.season] || 0) + 1;
             return acc;
         },{});    
@@ -13,7 +13,7 @@ csvToJson().fromFile("../Data/matches.csv").then(source => {
         fs.writeFileSync("../public/output/1-matches-per-year.json", JSON.stringify(matchesData,null,2));
         
     }  
-    matchesPerYear(source);  
+    matchesPerYear(matches);  
 });
 
 
--- server/2-matches-won-per-team-per-year.js
+++ server/2-matches-won-per-team-per-year.js
@@ -2,9 +2,9 @@
 const csvToJson = require("csvtojson");
 const fs= require("fs");
 
-csvToJson().fromFile("../Data/matches.csv").then(source => {
-    function wonTeamMatchesPerYear(source){
-        const matchesWonData = source.reduce((acc, match) => {
+csvToJson().fromFile("../Data/matches.csv").then(matches => {
+    function wonTeamMatchesPerYear(matches){
+        const matchesWonData =matches.reduce((acc, match) => {
             if (match.winner) {
                 acc[match.season] = acc[match.season] || {};
                 acc[match.season][match.winner] = (acc[match.season][match.winner] || 0) + 1;
@@ -15,7 +15,7 @@ csvToJson().fromFile("../Data/matches.csv").then(source => {
         console.log(matchesWonData);
          fs.writeFileSync("../public/output/2-matches-won-per-team-per-year.json", JSON.stringify(matchesWonData,null,2));
     }
-    wonTeamMatchesPerYear(source);
+    wonTeamMatchesPerYear(matches);
    
 });
 
